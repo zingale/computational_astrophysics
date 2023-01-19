4df9e91364a5245301f7fb133eca25969c88cebe
Michael Zingale <michael.zingale@stonybrook.edu>
2023-01-19 16:44:35 -0500

    another test

--- examples/linear-algebra/gauss_test.cpp
+++ examples/linear-algebra/gauss_test.cpp
@@ -16,4 +16,27 @@ int main() {
     for (auto e : x) {
         std::cout << e << std::endl;
     }
+
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+    // next example
+
+    Array A2{{{0, 0, 0, 4},
+              {0, 0, 3, 0},
+              {5, 6, 7, 8},
+              {0, 4, 3, 2}}};
+
+    std::vector<double> b2{5, 4, 9, 1};
+
+    auto x2 = gauss_elim(A2, b2);
+
+    for (auto e : x2) {
+        std::cout << e << std::endl;
+    }
+
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+
 }
