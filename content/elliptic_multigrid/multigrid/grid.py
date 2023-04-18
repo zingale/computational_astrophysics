76ae8c99f8bce4ce8c2d2c7098626f4104dffc3c
Michael Zingale <michael.zingale@stonybrook.edu>
2023-04-18 08:52:53 -0400

    cleaning before class

--- content/elliptic_multigrid/multigrid/grid.py
+++ content/elliptic_multigrid/multigrid/grid.py
@@ -1,6 +1,8 @@
 import numpy as np
 
+
 class Grid:
+
     def __init__(self, nx, ng=1, xmin=0, xmax=1,
                  bc_left_type="dirichlet", bc_left_val=0.0,
                  bc_right_type="dirichlet", bc_right_val=0.0):
@@ -39,20 +41,12 @@ class Grid:
         return np.sqrt(self.dx * np.sum(e[self.ilo:self.ihi+1]**2))
 
     def compute_residual(self):
-        """compute and store the residual"""
+        """compute and return the residual"""
+
         self.r[self.ilo:self.ihi+1] = self.f[self.ilo:self.ihi+1] - \
             (self.v[self.ilo+1:self.ihi+2] -
              2 * self.v[self.ilo:self.ihi+1] +
              self.v[self.ilo-1:self.ihi]) / self.dx**2
-
-    def residual_norm(self):
-        """compute the residual norm"""
-        self.compute_residual()
-        return self.norm(self.r)
-
-    def source_norm(self):
-        """compute the source norm"""
-        return self.norm(self.f)
 
     def fill_bcs(self):
         """fill the boundary conditions on phi"""
@@ -75,8 +69,10 @@ class Grid:
         else:
             raise ValueError("invalid bc_right_type")
 
-    def restrict(self, comp="v"):
+    def restrict(self, a):
         """restrict the data to a coarser (by 2x) grid"""
+
+        assert len(a) == self.nx + 2 * self.ng
 
         # create a coarse array
         ng = self.ng
@@ -87,35 +83,18 @@ class Grid:
 
         coarse_data = np.zeros((nc + 2*ng), dtype=np.float64)
 
-        if comp == "v":
-            fine_data = self.v
-        elif comp == "f":
-            fine_data = self.f
-        elif comp == "r":
-            fine_data = self.r
-        else:
-            raise ValueError("invalid component")
-
-        coarse_data[ilo_c:ihi_c+1] = 0.5 * (fine_data[self.ilo:self.ihi+1:2] +
-                                            fine_data[self.ilo+1:self.ihi+1:2])
+        coarse_data[ilo_c:ihi_c+1] = 0.5 * (a[self.ilo:self.ihi+1:2] +
+                                            a[self.ilo+1:self.ihi+1:2])
 
         return coarse_data
 
-    def prolong(self, comp="v"):
+    def prolong(self, a):
         """prolong the data in the current (coarse) grid to a finer (factor
         of 2 finer) grid using linear reconstruction.
 
         """
 
-        if comp == "v":
-            coarse_data = self.v
-        elif comp == "f":
-            coarse_data = self.f
-        elif comp == "r":
-            coarse_data = self.r
-        else:
-            raise ValueError("invalid component")
-
+        assert len(a) == self.nx + 2 * self.ng
 
         # allocate an array for the coarsely gridded data
         ng = self.ng
@@ -128,15 +107,15 @@ class Grid:
 
         # slopes for the coarse data
         m_x = self.scratch_array()
-        m_x[self.ilo:self.ihi+1] = 0.5 * (coarse_data[self.ilo+1:self.ihi+2] -
-                                          coarse_data[self.ilo-1:self.ihi])
+        m_x[self.ilo:self.ihi+1] = 0.5 * (a[self.ilo+1:self.ihi+2] -
+                                          a[self.ilo-1:self.ihi])
 
         # fill the '1' children
         fine_data[ilo_f:ihi_f+1:2] = \
-            coarse_data[self.ilo:self.ihi+1] - 0.25 * m_x[self.ilo:self.ihi+1]
+            a[self.ilo:self.ihi+1] - 0.25 * m_x[self.ilo:self.ihi+1]
 
         # fill the '2' children
         fine_data[ilo_f+1:ihi_f+1:2] = \
-            coarse_data[self.ilo:self.ihi+1] + 0.25 * m_x[self.ilo:self.ihi+1]
+            a[self.ilo:self.ihi+1] + 0.25 * m_x[self.ilo:self.ihi+1]
 
         return fine_data
