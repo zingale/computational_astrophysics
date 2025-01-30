585fbc5537deeaa9f7b95af7037f50e6867c4875
Michael Zingale <michael.zingale@stonybrook.edu>
2025-01-30 13:12:20 -0500

    fix function call + use C++20 pi

--- examples/integration/simpsons.cpp
+++ examples/integration/simpsons.cpp
@@ -3,19 +3,21 @@
 #include <cmath>
 #include <functional>
 #include <cassert>
-#include <vector>
+#include <numbers>
 
 // An implementation of the composite Simpsons rule for integration.  This
 // assumes that we will use an even number of intervals
 
+// note: requires C++20
+
 // the function we will integrate
 double f(const double x) {
-    return 1.0 + x * std::sin(M_PI * x) / 4.0;
+    return 1.0 + x * std::sin(std::numbers::pi * x) / 4.0;
 }
 
 // the analytic integral of f(x) over [1/2, 3/2]
 double I_analytic() {
-    return 1.0 - 0.5 / std::pow(M_PI, 2);
+    return 1.0 - 0.5 / std::pow(std::numbers::pi, 2);
 }
 
 // implement the composite simpsons rule.  Here N is the number
@@ -30,7 +32,7 @@ double simpsons(const double xmin, const double xmax, const int N,
 
     for (int n = 0; n < N; n += 2) {
         double x0 = xmin + static_cast<double>(n) * dx;
-        I += dx / 3.0 * (f(x0) + 4.0 * f(x0 + dx) + f(x0 + 2.0*dx));
+        I += dx / 3.0 * (func(x0) + 4.0 * func(x0 + dx) + func(x0 + 2.0*dx));
     }
 
     return I;
@@ -39,12 +41,10 @@ double simpsons(const double xmin, const double xmax, const int N,
 
 int main() {
 
-    std::vector<int> bins{2, 4, 8, 16, 32, 64};
-
     double xmin{0.5};
     double xmax{1.5};
 
-    for (auto N : bins) {
+    for (auto N : {2, 4, 8, 16, 32, 64}) {
         double I = simpsons(xmin, xmax, N, f);
         double err = std::abs(I - I_analytic());
         std::cout << std::setw(3) << N << ": " << err << std::endl;
