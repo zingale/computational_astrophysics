3d5352291f42f07bc70b33b6b08ae1d876493be5
Michael Zingale <michael.zingale@stonybrook.edu>
2025-02-09 10:39:08 -0500

    clean-up

--- examples/integration/blackbody.cpp
+++ examples/integration/blackbody.cpp
@@ -3,7 +3,6 @@
 #include <cmath>
 #include <cassert>
 #include <functional>
-#include <vector>
 
 // example of integrating to infinity.  We will integrate
 //
@@ -12,7 +11,7 @@
 // from 0 to infinity.  This arises from integrating the Planck function over
 // all wavelengths.
 
-const double SMALL = 1.e-30;
+const double SMALL{1.e-30};
 
 inline double zv(const double x, const double c) {
     return x / (c + x);
@@ -22,22 +21,13 @@ inline double xv(const double z, const double c) {
     return c * z / (1.0 - z + SMALL);
 }
 
-
-double gaussian(const double x) {
-    return std::exp(-x*x);
-}
-
-double gaussian_analytic() {
-    return std::sqrt(M_PI) / 2;
-}
-
 double integrand(const double x) {
     double I{0};
 
     if (x == 0) {
         I = 0.0;
     } else {
-        I = std::pow(x, 3) / (std::exp(x) - 1.0);
+        I = std::pow(x, 3) / std::expm1(x);
     }
     return I;
 }
@@ -80,48 +70,11 @@ double simpsons_inf(const int N,
 
 }
 
-
-// implement the composite trapezoid rule.  Here N is the number
-// of intervals, so there will be N+1 points from xmin to xmax
-// this version is written to integrate from [0, oo]
-double trap_inf(const int N,
-                std::function<double(const double)> func) {
-
-    double I = 0.0;
-    double c = 3.0;
-
-    // we will work in the transformed coords z = [0, 1]
-    double dz = (1.0 - 0.0) / static_cast<double>(N);
-
-    for (int n = 0; n < N; ++n) {
-        double z0 = static_cast<double>(n) * dz;
-        double z1 = static_cast<double>(n+1) * dz;
-
-        double f0 = func(xv(z0, c));
-        double f1 = func(xv(z1, c));
-
-        I += 0.5 * dz * (f0 / std::pow(1.0 - z0 + SMALL, 2) +
-                         f1 / std::pow(1.0 - z1 + SMALL, 2));
-    }
-
-    I *= c;
-
-    return I;
-
-}
-
 int main() {
 
-    std::vector<int> bins{2, 4, 8, 16, 32, 64};
-
-    for (auto N : bins) {
+    for (auto N : {2, 4, 8, 16, 32, 64, 128}) {
         auto I = simpsons_inf(N, integrand);
         double err = std::abs(I - I_analytic());
-
-        // uncomment these lines to test the integration
-        // of a Gaussian over [0, oo]
-        //auto I = simpsons_inf(N, gaussian);
-        //double err = std::abs(I - gaussian_analytic());
 
         std::cout << std::setw(3) << N << ": " << I << " " << err << std::endl;
     }
