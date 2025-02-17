2b0ba83c7211e6627bc8d94a81ed54af908c45d9
Michael Zingale <michael.zingale@stonybrook.edu>
2025-02-17 08:32:10 -0500

    a little cleaning of the C++ code

--- examples/ODEs/adaptive_RK4/orbit_adaptive.cpp
+++ examples/ODEs/adaptive_RK4/orbit_adaptive.cpp
@@ -113,8 +113,8 @@ public:
 
         // kinetic energy
 
-        double KE = 0.5 * (std::pow(state.u, 2) + std::pow(state.v, 2));
-        double PE = -GM / std::sqrt(std::pow(state.x, 2) + std::pow(state.y, 2));
+        double KE = 0.5 * (state.u * state.u + state.v * state.v);
+        double PE = -GM / std::sqrt(state.x * state.x + state.y * state.y);
 
         return KE + PE;
     }
@@ -155,16 +155,17 @@ public:
         state_temp = state_old + dt * ydot3;
         auto ydot4 = rhs(state_temp);
 
-        OrbitState s = state_old + (dt / 6.0) * (ydot1 + 2.0 * ydot2 + 2.0 * ydot3 + ydot4);
+        OrbitState s = state_old + (dt / 6.0) *
+            (ydot1 + 2.0 * ydot2 + 2.0 * ydot3 + ydot4);
 
         return s;
     }
 
 
     void integrate(const double dt_in, const double err, const double tmax) {
-        // integrate the equations of motion using 4th order R-K method with an
-        // adaptive stepsize, to try to achieve the relative error err.  dt here
-        // is the initial timestep
+        // integrate the equations of motion using 4th order R-K
+        // method with an adaptive stepsize, to try to achieve the
+        // relative error err.  dt here is the initial timestep
 
         // if err < 0, then we don't do adaptive stepping, but rather
         // we always walk at the input dt
@@ -177,7 +178,7 @@ public:
 
         while (t < tmax) {
 
-            auto &state_old = history.back();
+            const auto &state_old = history.back();
 
             // adaptive stepping iteration loop -- keep trying to take
             // a step until we achieve our desired error
@@ -200,20 +201,27 @@ public:
 
                 auto state_single = single_step(state_old, dt);
 
-                // state_new should be more accurate than state_single since it
-                // used smaller steps
+                // state_new should be more accurate than state_single
+                // since it used smaller steps
 
                 // estimate the relative error now
 
-                double pos_err = std::max(std::abs((state_new.x - state_single.x) / state_single.x),
-                                          std::abs((state_new.y - state_single.y) / state_single.y));
+                double pos_err =
+                    std::max(std::abs((state_new.x - state_single.x) /
+                                      state_single.x),
+                             std::abs((state_new.y - state_single.y) /
+                                      state_single.y));
 
-                double vel_err = std::max(std::abs((state_new.u - state_single.u) / state_single.u),
-                                          std::abs((state_new.v - state_single.v) / state_single.v));
+                double vel_err =
+                    std::max(std::abs((state_new.u - state_single.u) /
+                                      state_single.u),
+                             std::abs((state_new.v - state_single.v) /
+                                      state_single.v));
 
                 rel_error = std::max(pos_err, vel_err);
 
-                // adaptive timestep algorithm from Garcia (Eqs. 3.30 and 3.31)
+                // adaptive timestep algorithm from Garcia (Eqs. 3.30
+                // and 3.31)
 
                 double dt_est = dt * std::pow(std::abs(err/rel_error), 0.2);
                 dt_new = std::min(std::max(0.9 * dt_est, 0.25 * dt), 4.0 * dt);
@@ -229,8 +237,8 @@ public:
             // successful step
             t += dt;
 
-            // store the new solution -- this will automatically be used as the
-            // "old" state in the next step
+            // store the new solution -- this will automatically be
+            // used as the "old" state in the next step
 
             time.push_back(t);
             history.push_back(state_new);
@@ -247,7 +255,8 @@ int main() {
     o.integrate(0.05, 1.e-5, 1.0);
 
     std::cout << "number of points = " << o.npts() << std::endl;
-    std::cout << "number of times a step was rejected = " << o.get_n_reset() << std::endl;
+    std::cout << "number of times a step was rejected = "
+              << o.get_n_reset() << std::endl;
 
     std::ofstream of("orbit_adaptive.dat");
 
