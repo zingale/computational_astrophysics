e354f2e9604ee5bfa5d311e197ac9c942b18990f
Michael Zingale <michael.zingale@stonybrook.edu>
2025-03-13 09:02:19 -0400

    remove the 5th planet -- it gets ejected!

--- examples/planetary_stability/radial_velocity.cpp
+++ examples/planetary_stability/radial_velocity.cpp
@@ -4,7 +4,9 @@
 #include <fstream>
 #include <iomanip>
 #include <iostream>
+#include <ranges>
 #include <vector>
+#include <print>
 
 #include "solar_system.H"
 
@@ -12,8 +14,8 @@ int main() {
 
     constexpr double M_star{0.3};
 
-    std::vector<double> m_planets{1.e-3, 4.e-5, 3.e-5, 1.e-5, 0.5e-5};
-    std::vector<double> a_planets{2, 1, 0.5, 0.2, 0.1};
+    std::vector<double> m_planets{1.e-3, 4.e-5, 3.e-5, 1.e-5}; //, 0.5e-5};
+    std::vector<double> a_planets{2, 1, 0.5, 0.2}; // , 0.1};
 
     SolarSystem s(M_star, m_planets, a_planets, 1000);
 
@@ -26,8 +28,12 @@ int main() {
     std::cout << "initial hill separation = "
               << s.compute_hill_separation(s.history[0]) << std::endl;
 
-    double max_P = std::sqrt(std::pow(a_planets[0], 3) / M_star);
-    std::cout << "maximum period = " << max_P << std::endl;
+    double max_P{-1.0};
+    for (const auto [n, a] : a_planets | std::views::enumerate) {
+        double P = std::sqrt(std::pow(a, 3) / M_star);
+        max_P = std::max(max_P, P);
+        std::println("planet {} period = {:7.3f} years", n, P);
+    }
 
     // observe once a week
     s.integrate(1.0/52.0, 25 * max_P);
@@ -42,4 +48,25 @@ int main() {
         of << std::setw(14) << s.times[n];
         of << std::setw(14) << states[0].u << std::endl;
     }
+
+    std::ofstream of2("planet_positions.dat");
+
+    for (std::size_t n = 0; n < s.history.size(); ++n) {
+        const auto& states = s.history[n];
+
+        // only output the radial velocity of the star
+        // (which we'll take as just the x-velocity)
+        of2 << std::setw(14) << s.times[n];
+        for (const auto [n, p] : states | std::views::enumerate) {
+            if (n == 0) {
+                // skip the star
+                continue;
+            }
+
+            of2 << std::setw(14) << p.x << " ";
+            of2 << std::setw(14) << p.y << " ";
+        }
+        of2 << std::endl;
+    }
+
 }
